284fa3c19bb4050f5eaf3fd958cd72783192ad6d
shyam <shyam20998@gmail.com>
2023-02-12 12:24:22 +0530

    added search in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import { StyledNav } from "./Header.styled";
+import axios from "../../axios";
+import requests from "../../Requests";
+import { FaSearch } from "react-icons/fa";
 
 const Navigation = () => {
+	const searchRef = useRef("");
+	const [movies, setMovies] = useState("");
+
+	const searchHandler = async () => {
+		let searchQuery = searchRef.current.value;
+
+		const request = await axios.get(
+			`${requests.searchMovie}&query=${searchQuery}&page=1`
+		);
+		setMovies(request.data.results);
+	};
+
 	return (
 		<StyledNav>
 			<div className="nav__logo">HomeShow</div>
+			<div className="nav__search">
+				<input ref={searchRef} placeholder="Search" />
+				<button onClick={searchHandler}>
+					<FaSearch />
+				</button>
+			</div>
 		</StyledNav>
 	);
 };
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 export const StyledNav = styled.header`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 	position: fixed;
 	top: 0;
-	padding: 20px 30px;
+	padding: 20px 0;
 	width: 100%;
 	height: 50px;
 	z-index: 1;
@@ -19,5 +22,32 @@ export const StyledNav = styled.header`
 		color: orange;
 		font-weight: 700;
 		font-size: 30px;
+		margin-left: 30px;
+	}
+
+	.nav__search {
+		margin-right: 30px;
+		display: flex;
+
+		input {
+			outline: none;
+			padding: 10px;
+			margin-right: -20px;
+			width: 100%;
+			border: none;
+			border-radius: 99px;
+		}
+
+		button {
+			background: transparent;
+			border: none;
+			padding: 0;
+			border-radius: 99px;
+			cursor: pointer;
+
+			&:hover {
+				color: #192133;
+			}
+		}
 	}
 `;
